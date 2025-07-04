bf6bca676522263d3c93e5f4f3da3635e233e997
Deepak-Bhosale <bhosaledeepak15@gmail.com>
2025-07-04 11:05:45 +0530

    checkoing without index

--- src/repositories/list/ListModel.ts
+++ src/repositories/list/ListModel.ts
@@ -5,4 +5,4 @@ import { IListModel } from "./IListModel";
 export const listSchema = new ListSchema({ collection: "lists", versionKey: false });
 export const listModel: mongoose.Model<IListModel> = mongoose.model<IListModel>("lists", listSchema);
 
-listModel.collection.createIndex({ title: 1 });
+// listModel.collection.createIndex({ title: 1 });
--- src/repositories/user/UserModel.ts
+++ src/repositories/user/UserModel.ts
@@ -1,15 +1,12 @@
-import * as mongoose from 'mongoose';
-import { UserSchema } from './UserSchema';
-import { IUserModel } from './IUserModel';
+import * as mongoose from "mongoose";
+import { UserSchema } from "./UserSchema";
+import { IUserModel } from "./IUserModel";
 
 export const userSchema = new UserSchema({
-  collections: 'users',
+  collections: "users",
   versionKey: false,
 });
 
-export const userModel: mongoose.Model<IUserModel> = mongoose.model<IUserModel>(
-  'users',
-  userSchema
-);
+export const userModel: mongoose.Model<IUserModel> = mongoose.model<IUserModel>("users", userSchema);
 
-userModel.collection.createIndex({ email: 1 });
+// userModel.collection.createIndex({ email: 1 });
